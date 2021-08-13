ff9ad62ce043fd96e51b6883e2d92c5c5068dee8
fagneralves <fagner.all@hotmail.com>
2021-08-13 16:29:11 -0300

    Praticas de Strings

---
+++ strings.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+#Comentario Printar string
+
+print("Olá Mundo")
+print('\n') # Quebra de Linha
+print("""String multi-linhas 
+que se quebra automaticamente
+""")#Multi linha
+print("Concatena"+"ção de strings") #Concatenação
+print(len("Olá Mundo")) #Numero de letras
