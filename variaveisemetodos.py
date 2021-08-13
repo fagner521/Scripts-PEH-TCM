e1826260152620f7fd306850cceed697c07ca72e
fagneralves <fagner.all@hotmail.com>
2021-08-13 16:47:19 -0300

    Aula de variáveis e métodos

---
+++ variaveisemetodos.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+#Variaveis e metodos
+
+frase = "Estou acompanhando o curso"
+print(frase.upper()) #Letra maiuscula
+print(frase.lower()) #Letra minuscula
+print(frase.title()) #frase tipo titulo
+
+print(len(frase)) #Numero de caracteres
+
+nome = "Fagner"
+anos = 80
+altura = 5.6
+
+print(int(anos))
+print(int(30.9))
+
+print("Meu nome é %s e eu tenho %s anos e %s de altura" %(nome, anos, altura))
+
+anos += 2
+print (anos)
