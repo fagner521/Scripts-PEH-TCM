640462dd80fd101a82974ca375a0cb701c871805
fagneralves <fagner.all@hotmail.com>
2021-08-13 16:35:05 -0300

    Aula de operações matematicas

---
+++ matematica.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#Pratica de operações matematicas
+
+print(25 + 25) #adição
+print(30 - 25) #subtração
+print(5 * 10) #multiplicação
+print(7 / 2) #divisão
+print(50 + 50 - 50 * 50 / 50)
+print(5 ** 2) #exponenciação
+print(7 % 2) #sobra de divisão
+print(7 // 2) #divisão sem sobras
