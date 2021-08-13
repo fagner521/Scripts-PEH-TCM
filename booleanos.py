c267947f6adfb5ee31b4bb91c24cb698be8fca5a
fagneralves <fagner.all@hotmail.com>
2021-08-13 18:03:46 -0300

    Aula de expressões booleanas.

---
+++ booleanos.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+print("Explorando expressões booleanas")
+
+booleano1 = True
+booleano2 = 3*3 == 9
+booleano3 = False
+booleano4 = 3*3 == 8
+
+print(booleano1, booleano2, booleano3, booleano4)
+print(type(booleano1))
